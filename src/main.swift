2f3b092f435ab6b795c33994047f44978bc8b772
repl.it user <replituser@example.com>
2021-01-29 03:54:44 +0000

    Init Code

---
+++ src/main.swift
@@ -0,0 +1,15 @@
+let name: String = "Test"
+var age: Int = 1000
+var job = "IOS.Dev"
+let pie = 3.14
+
+print(name)
+print(age)
+print(job)
+print(pie)
+
+age = 100
+job = "Designer"
+
+print(age)
+print(job)
