37dc9315f4f55d6414b6d84dd64a42ec4b02330e
iamiks <rmstjr1030@naver.com>
2021-01-30 21:08:01 +0900

    Update func.swift

--- src/func.swift
+++ src/func.swift
@@ -13,3 +13,11 @@ func introduce(myName: String, friendName: String) -> String {
 	return "내 이름은 \(myName)이고, 내 친구 이름은 \(friendName)이다."
 }
 print(introduce(myName: "강하늘", friendName: "전지현"))
+
+func writeEmail(from: String, to: String..., text: String) -> String {
+	var toStr: String = ""
+	for user in to {
+		toStr += ", \(user)"
+	}
+	return "\(from)님이 \(toStr)에게 '\(text)'라고 이메일을 전송 했습니다."
+}
